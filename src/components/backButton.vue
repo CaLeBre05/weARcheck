683024b60f570705b02ac35adcf70b655b5252c5
Felix478 <stoecklfe79272@th-nuernberg.de>
2022-05-05 19:31:27 +0200

    Create backButton.vue

---
+++ src/components/backButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="button" class="backButton" value="<">
+</template>
+
+<style scoped>
+.backButton {
+    text-align: center;
+    border-radius: 50px;
+    border: none;
+    background-color:lightgray;
+    position: absolute;
+}
+</style>
+
+<script>
+export default{
+    name: "backButton"
+};
+
+</script>
