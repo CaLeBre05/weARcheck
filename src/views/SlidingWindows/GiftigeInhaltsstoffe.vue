7d714b70f14ba731635a8be2a7770c15d0da383d
Felix478 <stoecklfe79272@th-nuernberg.de>
2022-05-22 15:01:20 +0200

    Update
    Work on Giftige Inhaltsstoffe with FoldoutBox included

--- src/views/SlidingWindows/GiftigeInhaltsstoffe.vue
+++ src/views/SlidingWindows/GiftigeInhaltsstoffe.vue
@@ -5,8 +5,55 @@
       :image="'schadstoffe_kleidung.jpg'"
       :color="'#34a0a4'"
       :textColor="'#FFFFFF'"
-      ><div class="flexContainerInhaltsstoffe"></div
-    ></PageWrapper>
+      ><div class="flexContainerInhaltsstoffe">
+        <p class="einleitung">
+          In Textilien und Kleidung stecken oft Schadstoffe wie Chemikalien und
+          Allergene, die wir wissentlich nicht an unsere Haut lassen würden. Die
+          Gifte gefährden zudem Arbeiter*innen und verseuchen die Abwässer im
+          Produktionsprozess.
+        </p>
+        <FoldoutBox id="firstFoldout-Inhaltsstoffe">
+          <template #boxTitle> Gift in der Lederherstellung </template>
+          <template #boxText>
+            Die meisten Schuhe werden noch immer aus Leder gefertigt. Der
+            Großteil des weltweit produzierten Leders wird mit hochgiftigen
+            Chemikalien gegerbt und behandelt. Die am häufigsten verwendeten
+            Chrom-III-Salze sind vor allem für die Umwelt bedenklich: Die
+            entstehende Schlacke kann dem Wasserkreislauf nicht wieder zugeführt
+            werden und wird noch viel zu selten recycelt. Viel zu häufig landen
+            sie gemeinsam mit gelösten Schwermetallen in der Umwelt. Unter
+            bestimmten Bedingungen können sich auch im Leder
+            Chrom-VI-Verbindungen bilden. Chrom VI ist meist krebserregend,
+            wirkt erbgutverändernd und als starkes Allergen. Es gefährdet nicht
+            nur die Arbeiter in der Herstellung, sondern kann über Hautkontakt
+            auch in den Körper des Trägers gelangen.
+          </template>
+        </FoldoutBox>
+        <FoldoutBox id="secondFoldout-Inhaltsstoffe">
+          <template #boxTitle> Schadstoffhaltige Färbung </template>
+          <template #boxText
+            >Das Kleine Schwarze ist ein Klassiker im Kleiderschrank. Dumm nur,
+            dass schwarz die mit Abstand am stärksten mit Schadstoffen belastete
+            Färbung in Kleidung und Textilien ist. Ist diese Färbung
+            minderwertig und kann sie sich, wie zum Beispiel bei Kunstfaser
+            häufig, nicht richtig mit dem Stoff verbinden, wird sie bei
+            Hautkontakt durch Schweiß leicht ausgewaschen und gelangt in den
+            Körper. Dort kann die Farbe Hautreizungen und sogar Allergien
+            hervorrufen.</template
+          >
+        </FoldoutBox>
+        <FoldoutBox id="thirdFoldout-Inhaltsstoffe">
+          <template #boxTitle> Krebserregende Motivdrucke </template>
+          <template #boxText
+            >In den auf T-Shirts und Pullovern aufgedruckten Motiven stecken
+            nicht selten bedenkliche Schadstoffe – etwa verschiedene Weichmacher
+            oder polyzyklische aromatische Kohlenwasserstoffe (PAK), die teils
+            als krebserregend, teils als fortpflanzungsschädigend
+            gelten.</template
+          >
+        </FoldoutBox>
+      </div></PageWrapper
+    >
 
     <BackButton />
   </div>
@@ -15,10 +62,11 @@
 <script>
 import BackButton from "@/components/backButton.vue";
 import PageWrapper from "@/components/PageWrapper.vue";
+import FoldoutBox from "@/components/FoldoutBox.vue";
 
 export default {
   name: "GiftigeInhaltsstoffe",
-  components: { BackButton, PageWrapper },
+  components: { BackButton, PageWrapper, FoldoutBox },
 };
 </script>
 
@@ -28,16 +76,20 @@ export default {
   padding: 0;
 }
 
+.einleitung {
+  font-size: 18px;
+}
+
+#firstFoldout-Inhaltsstoffe,
+#secondFoldout-Inhaltsstoffe,
+#thirdFoldout-Inhaltsstoffe {
+  margin-top: 2em;
+}
 .flexContainerInhaltsstoffe {
   display: flex;
   flex-direction: column;
   height: 500px;
-  margin: 1em 1em 3.5em 1em;
-  padding-top: 1em;
-  padding-left: 0.5em;
-  padding-right: 0.5em;
-  padding-bottom: 0.5em;
-  justify-content: space-between;
-  border: 1px solid black;
+  margin: 1em 1.5em 3.5em 1.5em;
+  padding-top: 1em 0.5em 0.5em 0.5em;
 }
 </style>
