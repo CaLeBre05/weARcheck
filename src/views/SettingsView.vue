d658647da6dd3c96c847b4785352f2bc5d2ba4c2
Felix478 <stoecklfe79272@th-nuernberg.de>
2022-05-30 13:03:22 +0200

    Update SettingsView.vue
    added round toggle switch

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -7,9 +7,21 @@
       <div class="settingsElement shadow" id="element1">
         <div class="settingSummary">Benachrichtigungen</div>
         <hr />
-        <div class="setting">Push Benachrichtigungen</div>
+        <div class="setting">
+          Push Benachrichtigungen<span id="span1"
+            ><label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span> </label
+          ></span>
+        </div>
         <hr />
-        <div class="setting">E-Mail Benachrichtigungen</div>
+        <div class="setting">
+          E-Mail Benachrichtigungen<span id="span2"
+            ><label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span> </label
+          ></span>
+        </div>
         <hr />
       </div>
       <div class="settingsElement shadow" id="element2">
@@ -35,6 +47,65 @@
 </template>
 
 <style>
+#span1,
+#span2 {
+  float: right;
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 20px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: 0.2s;
+  transition: 0.2s;
+}
+
+input:checked + .slider {
+  background-color: #000;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(20px);
+  -ms-transform: translateX(20px);
+  transform: translateX(20px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 hr {
   border: 1px solid lightgray;
   border-radius: 1px;
