8a43d4087abf130ba16ac11f52f763e209bfee05
CaLeBre05 <brennerca80665@th-nuernberg.de>
2022-06-26 14:29:32 +0200

    added firebase, webgl unity player

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { StreamBarcodeReader } from "vue-barcode-reader";
+import firebase from 'firebase/compat/app';
 
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCK5qFVXFdf3FuM9wyz5w2EAlAiJuSjf0Y",
+  authDomain: "wear-check.firebaseapp.com",
+  projectId: "wear-check",
+  storageBucket: "wear-check.appspot.com",
+  messagingSenderId: "146127385848",
+  appId: "1:146127385848:web:1105b64a47919a2894f70d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  StreamBarcodeReader,
   router,
   render: (h) => h(App),
 }).$mount("#app");
